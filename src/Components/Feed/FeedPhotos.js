dcca94b0b005acedbeeaf3c309b106b4f22646d3
Gabriel Rodrigues <jipacoding@gmail.com>
2020-12-04 13:58:51 -0400

    0821-feed-modal-1

--- src/Components/Feed/FeedPhotos.js
+++ src/Components/Feed/FeedPhotos.js
@@ -6,7 +6,7 @@ import Error from "../Helper/Error";
 import Loading from "../Helper/Loading";
 import styles from "./FeedPhotos.module.css";
 
-const FeedPhotos = () => {
+const FeedPhotos = ({ setModalPhoto }) => {
   const { data, loading, error, request } = useFetch();
 
   React.useEffect(() => {
@@ -25,7 +25,11 @@ const FeedPhotos = () => {
       <div>
         <ul className={`${styles.feed} animeLeft`}>
           {data.map((photo) => (
-            <FeedPhotosItem photo={photo} key={photo.id} />
+            <FeedPhotosItem
+              photo={photo}
+              key={photo.id}
+              setModalPhoto={setModalPhoto}
+            />
           ))}
         </ul>
       </div>
