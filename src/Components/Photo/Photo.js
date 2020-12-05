ad92a501030215e763550e9f36813abb906e4b16
Gabriel Rodrigues <jipacoding@gmail.com>
2020-12-05 12:11:22 -0400

    0832-perdeu-senha-1

--- src/Components/Photo/Photo.js
+++ src/Components/Photo/Photo.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import { PHOTO_GET_BY_ID } from "../../api";
+import { PHOTO_GET } from "../../api";
 import useFetch from "../../Hooks/useFetch";
 import Error from "../Helper/Error";
 import Loading from "../Helper/Loading";
@@ -11,7 +11,7 @@ const Photo = () => {
   const { data, loading, error, request } = useFetch();
 
   React.useEffect(() => {
-    const { url, options } = PHOTO_GET_BY_ID(id);
+    const { url, options } = PHOTO_GET(id);
     request(url, options);
   }, [id, request]);
 
--- src/api.js
+++ src/api.js
@@ -73,20 +73,10 @@ export function PHOTOS_GET({ page, total, user }) {
   };
 }
 
-export function PHOTO_GET_BY_ID(id) {
-  return {
-    url: `${API_URL}/api/photo/${id}`,
-    options: {},
-  };
-}
-
 export function PHOTO_GET(id) {
   return {
     url: `${API_URL}/api/photo/${id}`,
-    options: {
-      method: "GET",
-      cache: "no-store",
-    },
+    options: {},
   };
 }
 
@@ -112,6 +102,19 @@ export function PHOTO_DELETE(id) {
       headers: {
         Authorization: "Bearer " + window.localStorage.getItem("token"),
       },
+    },
+  };
+}
+
+export function PASSWORD_LOST(body) {
+  return {
+    url: API_URL + "/api/password/lost",
+    options: {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
     },
   };
 }
