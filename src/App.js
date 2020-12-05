dbda95fbc80bdd2cb4488bb04a5d973036c3b0f8
Gabriel Rodrigues <jipacoding@gmail.com>
2020-12-05 11:55:11 -0400

    0830-erro-404

--- src/App.js
+++ src/App.js
@@ -6,8 +6,10 @@ import Header from "./Components/Header";
 import ProtectedRoute from "./Components/Helper/ProtectedRoute";
 import Home from "./Components/Home";
 import Login from "./Components/Login/Login";
+import NotFound from "./Components/NotFound";
 import Photo from "./Components/Photo/Photo";
 import User from "./Components/User/User";
+import UserProfile from "./Components/User/UserProfile";
 import { UserStorage } from "./UserContext";
 
 function App() {
@@ -21,6 +23,8 @@ function App() {
             <Route path="login/*" element={<Login />} />
             <ProtectedRoute path="conta/*" element={<User />} />
             <Route path="foto/:id" element={<Photo />} />
+            <Route path="perfil/:user" element={<UserProfile />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
           <Footer />
         </UserStorage>
